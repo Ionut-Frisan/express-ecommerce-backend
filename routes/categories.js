f7d96670c8bc4f304e4eb22d9008f79373be9d2a
Ionut-Frisan <ionutfrisan@gmail.com>
2022-07-12 00:40:08 +0300

    protected routes and user permissions

--- routes/categories.js
+++ routes/categories.js
@@ -8,17 +8,22 @@ const {
   deleteCategory,
 } = require("../controllers/categories");
 
+const { protect, authorize } = require("../middleware/auth");
+
 const router = express.Router();
 
 // Re-route into other resource routers
 const productRouter = require("./products");
 router.use("/:categoryId/products", productRouter);
 
-router.route("/").get(getCategories).post(createCategory);
+router
+  .route("/")
+  .get(getCategories)
+  .post(protect, authorize("admin"), createCategory);
 router
   .route("/:id")
-  .get(getCategory)
-  .put(updateCategory)
-  .delete(deleteCategory);
+  .get(protect, authorize("admin"), getCategory)
+  .put(protect, authorize("admin"), updateCategory)
+  .delete(protect, authorize("admin"), deleteCategory);
 
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -6,6 +6,8 @@ const advancedResults = require("../middleware/advancedResults");
 // Import Model
 const Product = require("../models/Product");
 
+const { protect, authorize } = require("../middleware/auth");
+
 const {
   getProduct,
   getProducts,
@@ -20,8 +22,12 @@ const router = express.Router({ mergeParams: true });
 router
   .route("/")
   .get(/*advancedResults(Product, "category"),*/ getProducts)
-  .post(createProduct);
-router.route("/:id").get(getProduct).put(updateProduct).delete(deleteProduct);
-router.route("/:id/photo").put(productUploadPhoto);
+  .post(protect, authorize("admin"), createProduct);
+router
+  .route("/:id")
+  .get(getProduct)
+  .put(protect, authorize("admin"), updateProduct)
+  .delete(protect, authorize("admin"), deleteProduct);
+router.route("/:id/photo").put(protect, authorize("admin"), productUploadPhoto);
 
 module.exports = router;
